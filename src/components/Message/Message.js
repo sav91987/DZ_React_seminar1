859d761cb1670fc02ac1fc8eb21f848fb4568b23
Sotnikov_Andrey <sav91987@ya.ru>
2024-09-04 13:33:05 +0300

    added solution

---
+++ src/components/Message/Message.js
@@ -0,0 +1,11 @@
+import "./Message.css";
+
+function Message({ message }) {
+    return (
+        <div className="message">
+            <h1>{message}</h1>
+        </div>
+    );
+}
+
+export default Message;
